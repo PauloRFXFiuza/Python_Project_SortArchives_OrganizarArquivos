c6c72c6a67091582a9bab44ca0e3c9d710b829c0
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-28 11:45:16 -0300

    v.1.4

--- sortArchives.py
+++ sortArchives.py
@@ -2,3 +2,25 @@
 Sort Archives Project- By Paulo Fiuza- 12/28/2024
 Projeto Organizador de Arquivos
 '''
+
+'''The Python "os" library is used to interact with the operating system in a cross-platform way.
+A biblioteca "os" do Python é usada para interagir com o sistema operacional de forma multiplataforma.'''
+
+import os
+
+'''The tkinter.filedialog module is used to display dialog boxes related to file and directory handling in Tkinter, 
+which is the standard Python library for creating graphical user interfaces (GUIs). 
+It allows users to visually select files or folders.
+
+O módulo tkinter.filedialog é usado para exibir caixas de diálogo relacionadas à manipulação de 
+arquivos e diretórios no Tkinter, que é a biblioteca padrão do Python para criar interfaces gráficas (GUIs). 
+Ele permite que os usuários escolham arquivos ou pastas de maneira visual.'''
+
+'''
+The askdirectory method displays a dialog box that allows the user to select a directory (folder) from their file system.
+It returns the full path of the selected directory as a string.
+
+O método askdirectory exibe uma caixa de diálogo que permite ao usuário selecionar um diretório (pasta) em seu 
+sistema de arquivos. Ele retorna o caminho completo do diretório escolhido como uma string.'''
+
+from tkinter.filedialog import askdirectory
